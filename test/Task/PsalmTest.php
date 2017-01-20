d207de404e7667a8efde3a217acd13c955b20d13
Leon Weemen <weemen@leonweemen.nl>
2017-01-20 01:03:42 +0100

    Extended unittest

--- test/Task/PsalmTest.php
+++ test/Task/PsalmTest.php
@@ -4,8 +4,8 @@ namespace Weemen\GrumPHPPsalm;
 
 
 use GrumPHP\Collection\FilesCollection;
+use GrumPHP\Collection\ProcessArgumentsCollection;
 use GrumPHP\Configuration\GrumPHP;
-use GrumPHP\Formatter\ProcessFormatterInterface;
 use GrumPHP\Formatter\RawProcessFormatter;
 use GrumPHP\Process\AsyncProcessRunner;
 use GrumPHP\Process\ProcessBuilder;
@@ -13,6 +13,7 @@ use GrumPHP\Runner\TaskResult;
 use GrumPHP\Task\Context\GitPreCommitContext;
 use GrumPHP\Task\Context\RunContext;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Process\Process;
 use Weemen\GrumPHPPsalm\Task\Psalm;
 
 class PsalmTest extends \PHPUnit_Framework_TestCase
@@ -94,6 +95,7 @@ class PsalmTest extends \PHPUnit_Framework_TestCase
         $psalmTask  = $this->createPsalmTask();
         $result     = $psalmTask->run($context);
 
+        $this->assertInstanceOf(TaskResult::class, $result);
         $this->assertEquals(TaskResult::SKIPPED,$result->getResultCode());
     }
 
@@ -121,34 +123,137 @@ class PsalmTest extends \PHPUnit_Framework_TestCase
         $psalmTask  = $this->createPsalmTask();
         $result     = $psalmTask->run($context);
 
+        $this->assertInstanceOf(TaskResult::class, $result);
         $this->assertEquals(TaskResult::SKIPPED,$result->getResultCode());
     }
 
-//    /**
-//     * @test
-//     */
-//    public function it_will_run_process_with_success_handle()
-//    {
-//        $filesCollection = $this->getMockBuilder(FilesCollection::class)
-//            ->disableOriginalConstructor()
-//            ->getMock();
-//
-//        $filesCollection->expects($this->once())
-//            ->method('name')
-//            ->willReturn(new FilesCollection(['1.php']) );
-//
-//        $context = $this->getMockBuilder(GitPreCommitContext::class)
-//            ->disableOriginalConstructor()
-//            ->getMock();
-//
-//        $context->expects($this->once())
-//            ->method('getFiles')
-//            ->willReturn($filesCollection);
-//
-//        $psalmTask  = $this->createPsalmTask();
-//        $result     = $psalmTask->run($context);
-//
-//        $this->assertTrue($result->isPassed());
-//    }
+    /**
+     * @test
+     */
+    public function it_will_run_process_with_success_handle()
+    {
+        $process = $this->getMockBuilder(Process::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
+        $process->expects($this->any())
+            ->method('isSuccessful')
+            ->willReturn(true);
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects($this->once())
+            ->method('getParameter')
+            ->withAnyParameters()
+            ->willReturn(['psalm' => []]);
+
+        $processBuilder = $this->getMockBuilder(ProcessBuilder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $processBuilder->expects($this->any())
+            ->method('createArgumentsForCommand')
+            ->withAnyParameters()
+            ->willReturn(new ProcessArgumentsCollection());
+
+        $processBuilder->expects($this->any())
+            ->method('buildProcess')
+            ->withAnyParameters()
+            ->willReturn($process);
+
+        $asyncProcessRunner = $this->getMockBuilder(AsyncProcessRunner::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $formatter = $this->createMock(RawProcessFormatter::class);
+
+        $grumPHP    = new GrumPHP($container);
+        $psalmTask  = new Psalm($grumPHP, $processBuilder, $asyncProcessRunner, $formatter);
+
+
+        $filesCollection = $this->getMockBuilder(FilesCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $filesCollection->expects($this->once())
+            ->method('name')
+            ->willReturn(new FilesCollection(['1.php']) );
+
+        $context = $this->getMockBuilder(GitPreCommitContext::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $context->expects($this->once())
+            ->method('getFiles')
+            ->willReturn($filesCollection);
+
+        $result     = $psalmTask->run($context);
+
+        $this->assertInstanceOf(TaskResult::class, $result);
+        $this->assertTrue($result->isPassed());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_run_process_with_failed_handle()
+    {
+        $process = $this->getMockBuilder(Process::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $process->expects($this->any())
+            ->method('isSuccessful')
+            ->willReturn(false);
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects($this->once())
+            ->method('getParameter')
+            ->withAnyParameters()
+            ->willReturn(['psalm' => []]);
+
+        $processBuilder = $this->getMockBuilder(ProcessBuilder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $processBuilder->expects($this->any())
+            ->method('createArgumentsForCommand')
+            ->withAnyParameters()
+            ->willReturn(new ProcessArgumentsCollection());
+
+        $processBuilder->expects($this->any())
+            ->method('buildProcess')
+            ->withAnyParameters()
+            ->willReturn($process);
+
+        $asyncProcessRunner = $this->getMockBuilder(AsyncProcessRunner::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $formatter = $this->createMock(RawProcessFormatter::class);
+
+        $grumPHP    = new GrumPHP($container);
+        $psalmTask  = new Psalm($grumPHP, $processBuilder, $asyncProcessRunner, $formatter);
+
+
+        $filesCollection = $this->getMockBuilder(FilesCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $filesCollection->expects($this->once())
+            ->method('name')
+            ->willReturn(new FilesCollection(['1.php']) );
+
+        $context = $this->getMockBuilder(GitPreCommitContext::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $context->expects($this->once())
+            ->method('getFiles')
+            ->willReturn($filesCollection);
+
+        $result     = $psalmTask->run($context);
+
+        $this->assertInstanceOf(TaskResult::class, $result);
+        $this->assertFalse($result->isPassed());
+    }
 }
